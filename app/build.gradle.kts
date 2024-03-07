05f5322f3c921414862898b7b3fb17cdf33eead8
Kekuz <anton.edlenko@yandex.ru>
2024-03-07 14:51:24 +0300

    Добавил rx в зависимости

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,9 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")}
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("io.reactivex:rxandroid:1.2.1")
+    implementation("io.reactivex:rxjava:1.1.6")
+
+}
